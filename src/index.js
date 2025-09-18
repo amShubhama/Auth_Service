421b227aeb1e48db36d48fc381a79787a1aa0b19
amShubhama <shubham14sk32@gmail.com>
2025-09-18 23:59:28 +0530

    Initial Setup

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const { PORT } = require('./config/serverConfig.js');
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("API is working");
+})
+
+
+const prepareAndStartServer = () => {
+    app.listen(PORT, () => {
+        console.log(`Server is started on ${PORT}`);
+    })
+}
+
+prepareAndStartServer();
