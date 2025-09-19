524018f7baaab7f46c6e6d7787c9c2734128cedc
amShubhama <shubham14sk32@gmail.com>
2025-09-19 12:12:58 +0530

    Signup logic with password encryption done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const { PORT } = require('./config/serverConfig.js');
+const apiRoutes = require('./routes/index.js');
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("API is working");
-})
-
-
 const prepareAndStartServer = () => {
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.get('/', (req, res) => res.send("Api's is working!"));
+    app.use('/api', apiRoutes);
+
     app.listen(PORT, () => {
         console.log(`Server is started on PORT: ${PORT}`);
     })
